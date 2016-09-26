f198fb69587f2207c3fc37904ab7df81b2d942bc
Robin Radic <robin@radic.nl>
2016-09-26 23:11:19 +0200

    init

---
+++ spec/colors/ColorSpec.js
@@ -0,0 +1,12 @@
+"use strict";
+var __1 = require('../..');
+describe("Player", function () {
+    var consoleColors;
+    beforeEach(function () {
+        consoleColors = new __1.ServerConsoleColors();
+    });
+    it("should be awesome", function () {
+        expect(consoleColors.isAwesome()).toBe(true);
+    });
+});
+//# sourceMappingURL=ColorSpec.js.map
